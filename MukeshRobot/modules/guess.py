73aef2748c26c46337b681a6a91ae596cf786465
mukesh364 <90526750+mukesh364@users.noreply.github.com>
2022-08-02 01:51:49 +0530

    Update guess.py

--- MukeshRobot/modules/guess.py
+++ MukeshRobot/modules/guess.py
@@ -9,8 +9,8 @@ async def wish(e):
          mm = random.randint(1,100)
          lol = await e.get_reply_message()
          fire = "https://telegra.ph/file/3fe0f7dedb81528a57313.jpg"
-         await neko.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your Guess is {mm}%__**\n\n__Correct!!!", reply_to=lol)
+         await pbot.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your Guess is {mm}%__**\n\n__Correct!!!", reply_to=lol)
  if not e.is_reply:
          mm = random.randint(1,100)
          fire = "https://telegra.ph/file/3fe0f7dedb81528a57313.jpg"
-         await neko.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your Guess is {mm}%__**\n\n__Correct!!!", reply_to=e)
+         await pbot.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your Guess is {mm}%__**\n\n__Correct!!!", reply_to=e)
