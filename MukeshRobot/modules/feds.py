f0a3ebff19ddf3c77927adc70c0d1adff86ad2f2
mukesh364 <90526750+mukesh364@users.noreply.github.com>
2022-08-05 15:55:21 +0530

    Update feds.py

--- MukeshRobot/modules/feds.py
+++ MukeshRobot/modules/feds.py
@@ -2403,7 +2403,7 @@ def fed_user_help(update: Update, context: CallbackContext):
     )
 
 
-__mod_name__ = "Federations"
+__mod_name__ = "ғᴇᴅ"
 
 
 NEW_FED_HANDLER = CommandHandler("newfed", new_fed)
